fd056512fc4dabfd9df7e1dbd8c89decda25c841
Shekar Tippur <ctippur@Shekars-MacBook-Pro.local>
2025-05-30 17:46:33 -0700

    Initial commit

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -2,14 +2,7 @@ import { defineAuth } from '@aws-amplify/backend-auth';
 
 export const auth = defineAuth({
   loginWith: {
-    email: true,
-    oauth: {
-      google: {
-        clientId: 'YOUR_GOOGLE_CLIENT_ID',
-        clientSecret: 'YOUR_GOOGLE_CLIENT_SECRET',
-        scopes: ['profile', 'email', 'openid']
-      }
-    }
+    email: true
   },
   userAttributes: {
     profilePicture: {
