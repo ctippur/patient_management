e49d4d14c96344eba24716c164b8af5a671e7c1b
Shekar Tippur <ctippur@Shekars-MacBook-Pro.local>
2025-05-30 18:07:44 -0700

    Initial commit

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -3,11 +3,5 @@ import { defineAuth } from '@aws-amplify/backend-auth';
 export const auth = defineAuth({
   loginWith: {
     email: true
-  },
-  userAttributes: {
-    profilePicture: {
-      mutable: true,
-      required: false
-    }
   }
 });
